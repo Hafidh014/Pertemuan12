0015ac87bd99270ecf0f62007b9d9052ce8d425c
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-19 17:25:02 +0700

    memanggil fungsi utama

--- Tugas.cpp
+++ Tugas.cpp
@@ -46,3 +46,29 @@ public:
         }
     }
 };
+
+
+int main() {
+    RekeningBank* rekening[3];
+
+    rekening[0] = new RekeningBRI("Hafid", 5000000);
+    rekening[1] = new RekeningBRIPremium("Rida", 3000000);
+    rekening[2] = new RekeningBRIPremium("Bagas", 10000000);
+
+    cout << "Sebelum potong admin:\n";
+    for (int i = 0; i < 3; i++) {
+        rekening[i]->tampilkanInfo();
+    }
+
+    cout << "\nSetelah potong admin:\n";
+    for (int i = 0; i < 3; i++) {
+        rekening[i]->potongAdmin();
+        rekening[i]->tampilkanInfo();
+    }
+
+    for (int i = 0; i < 3; i++) {
+        delete rekening[i];
+    }
+
+    return 0;
+}
