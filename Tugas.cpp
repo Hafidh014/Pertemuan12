a1a574884b9c6b901910d5be939a5e8609c6afdb
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-19 17:18:09 +0700

    membuat class rekening bank

--- Tugas.cpp
+++ Tugas.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+class RekeningBank {
+protected:
+    string nama;
+    double saldo;
+
+public:
+    RekeningBank(string nama, double saldo) {
+        this->nama = nama;
+        this->saldo = saldo;
+    }
+
+
+};
